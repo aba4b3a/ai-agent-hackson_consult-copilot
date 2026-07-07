20361d4527c5ed5558a29c1eb7fbfc4666b2f225
rrhaya <321tagu.ram@gmail.com>
2026-07-07 11:58:49 +0900

    Implement knowledge formation screen

--- front/lib/schemas.ts
+++ front/lib/schemas.ts
@@ -146,6 +146,7 @@ export const dashboardMetricsSchema = z.object({
   sources_ingested: z.number(),
   observations_extracted: z.number(),
   entities_formed: z.number(),
+  relationships_created: z.number(),
   hypotheses_under_observation: z.number(),
   evidence_coverage: z.number(),
 });
